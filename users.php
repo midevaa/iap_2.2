578a70acc7c87252ccdb2c3c06114e0e4bcaeb21
midevaa <natmalwa77@gmail.com>
2025-09-16 22:29:08 +0300

    improved

--- users.php
+++ users.php
@@ -14,7 +14,6 @@ try {
 <head>
     <meta charset="UTF-8">
     <title>Users List</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <style>
         body {
             background-color: pink;
